b7f454f6cf88e79463d7b241be97c9c864be18bb
James Brand <James@InnoCreate.com>
2020-06-01 17:52:13 +0100

    keypoints dir

--- pose_camera.py
+++ pose_camera.py
@@ -176,9 +176,8 @@ def main():
 
         shadow_text(svg_canvas, 10, 20, text_line)
         for pose in outputs:
-            #for att in dir(pose):
-            #    print (att, getattr(pose,att))
-            print (pose.keypoints[0])
+            for att in dir(pose.keypoints):
+                print (att, getattr(pose.keypoints,att))
             draw_pose(svg_canvas, pose, src_size, inference_box)
         return (svg_canvas.tostring(), False)
 
