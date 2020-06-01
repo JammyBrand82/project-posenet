75972a32ffbf996a1760a8b65665864f25e1fa24
James Brand <James@InnoCreate.com>
2020-06-01 17:46:30 +0100

    dir

--- pose_camera.py
+++ pose_camera.py
@@ -176,7 +176,7 @@ def main():
 
         shadow_text(svg_canvas, 10, 20, text_line)
         for pose in outputs:
-            print(pose.__dict__)
+            print(pose.__dir__)
             draw_pose(svg_canvas, pose, src_size, inference_box)
         return (svg_canvas.tostring(), False)
 
